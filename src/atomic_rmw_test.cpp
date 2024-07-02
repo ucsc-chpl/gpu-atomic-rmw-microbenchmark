e5641eebb20fddad163445da9a671a88fd3955fd
gudhillon <gudhillon@gmail.com>
2024-07-02 14:29:23 -0700

    occupancy and iterations as additional args

--- src/atomic_rmw_test.cpp
+++ src/atomic_rmw_test.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
-#include "vk_utils.h"
+#include <getopt.h>
+#include "easyvk.h"
 
 #ifdef __ANDROID__
 #include <android/log.h>
@@ -16,54 +17,61 @@ using easyvk::Buffer;
 using easyvk::Program;
 using easyvk::vkDeviceType;
 
-extern "C" void atomic_rmw_microbenchmark(easyvk::Device device, uint32_t contention, uint32_t padding) { 
+uint32_t validate_kernel(easyvk::Buffer resultBuf, uint32_t rmw_iters, uint32_t num_trials, uint32_t contention, 
+                            uint32_t padding, uint32_t size) {
+    uint32_t error_count = 0;
+    for (int access = 0; access < size; access += padding) {
+            uint32_t observed_output = resultBuf.load<uint32_t>(access);
+            uint32_t expected_output = rmw_iters * num_trials * contention;
+            if (observed_output != expected_output) {
+                 error_count++;
+            }
+    }
+    return error_count;
+}
+
+extern "C" void atomic_rmw_microbenchmark(easyvk::Device device, uint32_t contention, uint32_t padding, uint32_t workgroups, uint32_t rmw_iters) { 
 
     uint32_t num_trials = 3;
-    uint32_t workgroup_size = device.properties.limits.maxComputeWorkGroupInvocations;
-    uint32_t workgroups = occupancy_discovery(device, workgroup_size, 256, get_spv_code("occupancy_discovery.cinit"), 3, 1024);
-    uint32_t global_work_size = workgroup_size * workgroups;
-    uint32_t buf_size = ((global_work_size) * padding) / contention;
-    vector<uint32_t> spv_code = get_spv_code("atomic_fa_relaxed.cinit");
+    uint32_t workgroup_size = device.properties.limits.maxComputeWorkGroupInvocations;    
+    uint32_t buf_size = ((workgroup_size * workgroups) * padding) / contention;
+    vector<uint32_t> spv_code =  
+    #include "atomic_fa_relaxed.cinit"
+    ;
 
-    cout << "\nRunning on " << device.properties.deviceName << ": workgroups (" << workgroup_size << ", 1) x " << workgroups << flush;
+   cout << "\nDevice: " << device.properties.deviceName << ", workgroups (" << workgroup_size << ", 1) x " << workgroups << endl
+     << "Running " << rmw_iters << " RMW iterations on contention " << contention
+     << ", padding " << padding << flush;
 
     Buffer result_buf = Buffer(device, buf_size, sizeof(uint32_t));
     Buffer rmw_iters_buf = Buffer(device, 1, sizeof(uint32_t));
-    Buffer strat_buf = Buffer(device, global_work_size, sizeof(uint32_t)); 
-    
-    for (int i = 0; i < global_work_size; i += 1) strat_buf.store<uint32_t>(i, (i / contention) * padding);
+    Buffer strat_buf = Buffer(device, workgroup_size * workgroups, sizeof(uint32_t)); 
 
-    uint32_t rmw_iters = 128; 
-    while(1) {
-        rmw_iters_buf.store<uint32_t>(0, rmw_iters);
-        result_buf.clear();
-        vector<Buffer> buffers = {result_buf, rmw_iters_buf, strat_buf};
+    result_buf.clear();
+    rmw_iters_buf.store<uint32_t>(0, rmw_iters);
+    for (int i = 0; i < workgroup_size * workgroups; i += 1) strat_buf.store<uint32_t>(i, (i / contention) * padding);
 
-        Program rmw_program = Program(device, spv_code, buffers);
-        rmw_program.setWorkgroups(workgroups);
-        rmw_program.setWorkgroupSize(workgroup_size);
-        rmw_program.initialize("rmw_test");
+    vector<Buffer> buffers = {result_buf, rmw_iters_buf, strat_buf};
 
-        float total_rate = 0.0;
-        float total_duration = 0.0;
-        for (int i = 1; i <= num_trials; i++) {
-            auto kernel_time = rmw_program.runWithDispatchTiming();
-            total_duration += (kernel_time / (double) 1000.0);
-            total_rate += ((static_cast<float>(rmw_iters) * workgroup_size * workgroups) / (kernel_time / (double) 1000.0)); 
-        }
-        if ((total_duration/num_trials) < 1000000.0) {
-            rmw_iters *= 2;
-            rmw_program.teardown();
-            if ((total_duration/num_trials) > 50000.0) cout << "." << flush;
-        } else {
-            cout << "\ncontention " << contention << ", padding " << padding << ": " << (total_rate / num_trials) << " atomic operations per microsecond" << endl;
-            rmw_program.teardown();
-            break;
-        }
+    Program rmw_program = Program(device, spv_code, buffers);
+    rmw_program.setWorkgroups(workgroups);
+    rmw_program.setWorkgroupSize(workgroup_size);
+    rmw_program.initialize("rmw_test");
+
+    float total_rate = 0.0;
+    float total_duration = 0.0;
+    for (int i = 1; i <= num_trials; i++) {
+        auto kernel_time = rmw_program.runWithDispatchTiming();
+        total_duration += (kernel_time / (double) 1000.0);
+        total_rate += ((static_cast<float>(rmw_iters) * workgroup_size * workgroups) / (kernel_time / (double) 1000.0)); 
+        cout << "." << flush;
     }
 
-    uint32_t error_count = validate_output(result_buf, rmw_iters, num_trials, contention, padding, buf_size);
-    cout << "kernel error count: " << error_count << endl;
+    cout << "\n\nThroughput: " << (total_rate / num_trials) << " atomic operations per microsecond" 
+     << "\nTime: " << (total_duration / num_trials) << " microseconds" 
+     << "\nKernel errors: " << validate_kernel(result_buf, rmw_iters, num_trials, contention, padding, buf_size) << endl;
+    
+    rmw_program.teardown();
     result_buf.teardown(); 
     rmw_iters_buf.teardown();
     strat_buf.teardown();
@@ -71,29 +79,40 @@ extern "C" void atomic_rmw_microbenchmark(easyvk::Device device, uint32_t conten
     return;
 }
 
-int main() {
-    cout << "Atomic RMW Microbenchmark: Contiguous Access, Atomic_Fetch_Add\n\n";
+int main(int argc, char* argv[]) {
     auto instance = easyvk::Instance(USE_VALIDATION_LAYERS);
-	auto physicalDevices = instance.physicalDevices();
+	auto physicalDevices = instance.physicalDevices(); 
+    uint32_t contention = 1, padding = 1, rmw_iters = 128;
+    uint32_t occupancy = 1;
+    uint32_t selected_device = 0;
 
-    vector<string> device_options;
-    for (size_t i = 0; i < physicalDevices.size(); i++) {
-        auto device = easyvk::Device(instance, physicalDevices.at(i));
-        device_options.push_back(device.properties.deviceName);
-        device.teardown();
+    if (argc == 1) {
+        cout << "Usage: program_name -o <occupancy> [-d <device>] [-c <contention>] [-p <padding>] [-i <rmw_iterations>]" << endl;
+        return 1;
     }
-
-    auto selected_devices = select_configurations(device_options, "Select devices:");
-
-    uint32_t contention = get_params("\nEnter a value for contention: ");
-    uint32_t padding = get_params("Enter a value for padding: ");
-
-    for (const auto& choice : selected_devices) {
-        auto device = easyvk::Device(instance, physicalDevices.at(choice));
-        atomic_rmw_microbenchmark(device, contention, padding);
-        device.teardown();
+    int opt;
+    while ((opt = getopt(argc, argv, "c:p:o:d:i:")) != -1) {
+        switch (opt) {
+            case 'c':
+                contention = static_cast<uint32_t>(stoi(optarg));
+                break;
+            case 'p':
+                padding = static_cast<uint32_t>(stoi(optarg));
+                break;
+            case 'o':
+                occupancy = static_cast<uint32_t>(stoi(optarg));
+                break;
+            case 'd':
+                selected_device = static_cast<uint32_t>(stoi(optarg));
+                break;
+            case 'i':
+                rmw_iters = static_cast<uint32_t>(stoi(optarg));
+                break;
+        }
     }
-
+    auto device = easyvk::Device(instance, physicalDevices.at(selected_device));
+    atomic_rmw_microbenchmark(device, contention, padding, occupancy, rmw_iters);
+    device.teardown();
     instance.teardown();
     return 0;
 }
